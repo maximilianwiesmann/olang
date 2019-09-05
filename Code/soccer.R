6b0fb947669de8363963e9aa896673a79919ee17
OrangeKatja <tanja.bien@gmail.com>
2019-09-06 01:08:59 +0200

    merged mcmc

--- Code/soccer.R
+++ Code/soccer.R
@@ -1,8 +1,8 @@
-p0 <- c(0.1, 0.8, 1)
+p0 <- c(0.1, 0.8, 1) # Behauptungen
 priori <- c(1/3, 1/3, 1/3)
 
-n <- 1000
-x <- 999
+n <- 1 # Anzahl Elfmeter
+x <- 1 # Anzahl Treffer
 
-post <- priori * dbinom(x, n, p0)
-post / sum(post)
+post <- priori * dbinom(x, n, p0) 
+post / sum(post) 
