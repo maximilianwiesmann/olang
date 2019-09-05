fbadb487ff2d8dcfbf044e84d7c7e18cf2afa99e
OrangeKatja <tanja.bien@gmail.com>
2019-09-05 14:42:16 +0200

    modified gitignore and BetaDistribution

--- Code/BetaDistribution.R
+++ Code/BetaDistribution.R
@@ -1,10 +1,11 @@
 library(stats)
 
 par(mfrow = c(3, 2)) # display multiple plots
-x = seq(0, 1, by = 0.01) # Probability
-alpha = c(0, 2, 10, 20, 50, 500)
-beta = c(0, 2, 8, 11, 27, 232)
+x = seq(0, 1, by = 0.001) # Probability
+alpha = c(1, 2, 0.5, 3, 5, 500)
+beta = c(1, 2, 0.5, 0.5, 2, 125.8)
 for(i in 1:length(alpha)){
   y <- dbeta(x, shape1 = alpha[i], shape2 = beta[i])
-  plot(x, y, type="l", xlab = "theta", ylab = "density")
+  plot(x, y, main = paste("alpha = ", alpha[i], ", beta = ", beta[i], sep = ""), 
+       type = "l", xlab = "theta", ylab = "density")
 }
