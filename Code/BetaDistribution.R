44ccdb542373e45e107f41081fbd5200a8814d1f
OrangeKatja <tanja.bien@gmail.com>
2019-09-05 23:01:54 +0200

    new images

--- Code/BetaDistribution.R
+++ Code/BetaDistribution.R
@@ -2,8 +2,8 @@ library(stats)
 
 par(mfrow = c(3, 2)) # display multiple plots
 x = seq(0, 1, by = 0.001) # Probability
-alpha = c(1, 2, 0.5, 3, 5, 500)
-beta = c(1, 2, 0.5, 0.5, 2, 125.8)
+alpha = c(1, 2, 0.5, 3, 5, 500000)
+beta = c(1, 2, 0.5, 0.5, 2, 125000.8)
 for(i in 1:length(alpha)){
   y <- dbeta(x, shape1 = alpha[i], shape2 = beta[i])
   plot(x, y, main = paste("alpha = ", alpha[i], ", beta = ", beta[i], sep = ""), 
